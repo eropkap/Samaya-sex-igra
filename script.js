bc980165b524fddac5a8d15277df0532993f4c4b
eropkap <eropkap@icloud.com>
2025-02-08 23:37:06 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
+let currentMode = "";
 let cards = {};
 
 // Загружаем карточки из JSON
-fetch('explicit_cards.json')
+fetch('updated_game_modes_silent.json')
     .then(response => response.json())
     .then(data => {
         cards = data;
     })
     .catch(error => console.error('Ошибка загрузки карточек:', error));
 
-function startGame() {
+function showModes() {
     document.querySelector('.container').classList.add('hidden');
+    document.getElementById('modeScreen').classList.remove('hidden');
+}
+
+function startGame(mode) {
+    currentMode = mode;
+    document.getElementById('modeScreen').classList.add('hidden');
     document.getElementById('gameScreen').classList.remove('hidden');
 }
 
 function spinWheel() {
-    let categories = Object.keys(cards);
-    let randomCategory = categories[Math.floor(Math.random() * categories.length)];
-    let cardList = cards[randomCategory];
+    let cardList = cards[currentMode];
     let randomCard = cardList[Math.floor(Math.random() * cardList.length)];
 
     let wheel = document.getElementById("wheel");
@@ -33,6 +38,18 @@ function spinWheel() {
 }
 
 function goBack() {
-    document.getElementById('gameScreen').classList.add('hidden');
+    document.querySelectorAll('.hidden').forEach(el => el.classList.add('hidden'));
     document.querySelector('.container').classList.remove('hidden');
+}
+
+function toggleSound() {
+    alert("Функция звука скоро появится!");
+}
+
+function toggleTheme() {
+    alert("Функция смены темы скоро появится!");
+}
+
+function setGameTime(minutes) {
+    alert("Игра установлена на " + minutes + " минут!");
 }
